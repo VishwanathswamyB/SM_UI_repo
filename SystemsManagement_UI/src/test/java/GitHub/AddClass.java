a0605f11ffb617b6ff42c6fc58a1b9c05ff12d2a
Computer <Computer@DESKTOP-S1II299.mshome.net>
2025-02-24 19:39:53 +0530

    Added a Class

---
+++ SystemsManagement_UI/src/test/java/GitHub/AddClass.java
@@ -0,0 +1,13 @@
+package GitHub;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class AddClass {
+	
+	@Test
+	public void demo() {
+		Reporter.log("Add Class", true);
+	}
+
+}
